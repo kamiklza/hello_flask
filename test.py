f9b6a2b80629a02931e52f04e5684454d841fad4
Kamiklza <kamiklza@hotmail.com>
2021-09-01 00:11:08 +0800

    final commit - added decorator to print function name, args and output result

--- test.py
+++ test.py
@@ -28,10 +28,25 @@ def is_authenticated_decorator(function):
 def create_blog_post(user, level):
     print(f"This is {user.name}'s new blog post. He is a {level} user.")
 
-new_user = User("Ken", level="vip")
+new_user = User("Ken", "vip")
 create_blog_post(new_user, level=new_user.level)
 
+#------------------Using decorator function to print function name, args and output result-------------------#
+def logging_decorator(fn):
+    def wrapper(*args, **kwargs):
+        print(f"You called {fn.__name__}{args}")
+        result = fn(args[0], args[1], args[2])
+        print(f"It returned: {result}")
 
+    return wrapper
+
+
+@logging_decorator
+def a_function(a, b, c):
+    return a * b * c
+
+
+a_function(1, 2, 3)
 
 
 
